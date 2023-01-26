31659352b0601656b0ebf219826fc1f08c76f7ff
Konneki <konnekdaniel@gmail.com>
2023-01-26 21:13:49 +0100

    Initial Commit

---
+++ Accessibiility/AccessibiilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibiilityApp.swift
+//  Accessibiility
+//
+//  Created by Daniel Konnek on 26.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibiilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
